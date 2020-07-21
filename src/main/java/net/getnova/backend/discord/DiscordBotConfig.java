bf97259fcbf1cc2fbf1394835eae758312988a00
Marcel Koch <koch_marcel@outlook.com>
2020-07-21 16:23:51 +0200

    Updated Config Value Descriptions

--- src/main/java/net/getnova/backend/discord/DiscordBotConfig.java
+++ src/main/java/net/getnova/backend/discord/DiscordBotConfig.java
@@ -9,10 +9,11 @@ import net.getnova.backend.config.ConfigValue;
 @Setter(AccessLevel.NONE)
 public class DiscordBotConfig {
 
-    @ConfigValue(id = "token", comment = "Discord Api Token")
+    @ConfigValue(id = "token", comment = {"The Discord Api token which should be used for the bot.",
+            "You can create or retrieve a token here: https://discord.com/developers/"})
     private String token = "";
 
-    @ConfigValue(id = "prefix", comment = {"Prefix for all commands, e.g. \"!\", \"?\", \".\".",
+    @ConfigValue(id = "prefix", comment = {"Prefix for all commands, e.g. '!', '?', '.'.",
             "Remember the you property have to put quotation marks around the prefix!"})
     private String prefix = "!";
 }
