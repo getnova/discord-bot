6f292402dbb05b853b3a59bfa077653247c6e3a4
Marcel Koch <koch_marcel@outlook.com>
2020-07-24 03:25:30 +0200

    Added support for moving of the bot while playing

--- src/main/java/net/getnova/backend/discord/feature/music/MusicEvent.java
+++ src/main/java/net/getnova/backend/discord/feature/music/MusicEvent.java
@@ -1,7 +1,9 @@
 package net.getnova.backend.discord.feature.music;
 
+import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.getnova.backend.discord.audio.AudioUtils;
 
@@ -22,5 +24,17 @@ final class MusicEvent extends ListenerAdapter {
     public void onGuildVoiceMove(final GuildVoiceMoveEvent event) {
         if (AudioUtils.isConnectedTo(event.getChannelLeft()) && event.getChannelLeft().getMembers().size() - 1 == 0)
             this.musicService.getPlayer(event.getGuild()).pause();
+    }
+
+    @Override
+    public void onGuildVoiceUpdate(final GuildVoiceUpdateEvent event) {
+        final VoiceChannel channelJoined = event.getChannelJoined();
+        if (channelJoined != null) {
+            if (channelJoined.getMembers().size() - 1 == 0) {
+                this.musicService.getPlayer(channelJoined.getGuild()).pause();
+            } else {
+                this.musicService.getPlayer(channelJoined.getGuild()).setVoiceChannel(channelJoined);
+            }
+        }
     }
 }
