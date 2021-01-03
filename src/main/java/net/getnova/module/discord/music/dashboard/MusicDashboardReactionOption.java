f3507849e8e42e022cc063a13d231a0e22b3a62e
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-01-03 14:44:01 +0100

    Improved GitHub Actions

--- src/main/java/net/getnova/module/discord/music/dashboard/MusicDashboardReactionOption.java
+++ src/main/java/net/getnova/module/discord/music/dashboard/MusicDashboardReactionOption.java
@@ -1,7 +1,6 @@
 package net.getnova.module.discord.music.dashboard;
 
 import discord4j.core.event.domain.message.MessageEvent;
-import discord4j.core.event.domain.message.ReactionAddEvent;
 import net.getnova.module.discord.music.GuildMusicManager;
 
 public interface MusicDashboardReactionOption {
